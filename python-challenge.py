9638054087e124271e80a91e921f42d462585eff
Gardner Guzman <codygardner36@gmail.com>
2021-07-01 23:12:14 -0400

    Python Add HW

---
+++ python-challenge.py
@@ -0,0 +1,25 @@
+ #Homework 2 PyPoll
+ #The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+ 
+ #First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+csvpath = os.path.join('PyPoll','Resources','election_data_csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+
+
