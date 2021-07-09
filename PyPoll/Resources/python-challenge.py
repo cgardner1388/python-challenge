e70c12e71285528b6d2ef5696adf7882ae52f6c5
Gardner Guzman <codygardner36@gmail.com>
2021-07-08 20:53:34 -0400

    Python HW

--- PyPoll/Resources/python-challenge.py
+++ PyPoll/Resources/python-challenge.py
@@ -11,41 +11,17 @@ import os
 
 #currentDirectory = os.getcwd()
 
-print(currentDirectory)
-
 # Module for reading CSV files
 import csv
 
-#csvpath = os.path.join('election_data.csv')
+csvpath = os.path.join('election_data.csv')
 
-csvpath = '/Users/gardnerguzman/Desktop/Georgia Tech Bootcamp/Homework/python-challenge/PyPoll/Resources/election_data.csv'
-
-votes = 0 
-
-#Come up with formula to find candidate and move on
-candidates =
-
-#Come up with formula 
-
-#csvpath = os.path.join('PyPoll','Resources','election_data.csv')
-
-with open(csvpath) as csvfile:
+with open('election_data.csv') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
     print(csvreader)
-#adding up the votes
-    for Row in csvreader:
-    #sum votes; make sure everything is sequential; can do everything in the same 4 loop
-        votes = votes + 1
-    
-print(votes)
-
-   
-
-
-
 
 
 
